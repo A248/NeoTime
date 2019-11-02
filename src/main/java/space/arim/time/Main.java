544989b0db9b68dd2ed24c605ebcc95765128a9c
ashto <ashto@DESKTOP-S85SKDN.fios-router.home>
2019-11-02 18:09:43 -0400

    made main class printed greeting string

---
+++ src/main/java/space/arim/time/Main.java
@@ -0,0 +1,22 @@
+package space.arim.time;
+
+import java.util.Scanner;
+
+public class Main {
+	static Scanner scanner;
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		scanner = new Scanner(System.in);
+		String mystring;
+		mystring= "iam Ashton";
+		System.out.println(mystring);
+		String u = scanner.next(); 
+		int w = u.length();
+		w += 5;
+		w = w + 6;
+		System.out.println(w);
+		scanner.close();
+	}
+
+}
